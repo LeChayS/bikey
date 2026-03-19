80bd553fa86d96d0709c14b4224e3bd1f16bcbea
Lê Thị Tuyết Trâm <142717464+LeChayS@users.noreply.github.com>
2026-03-19 10:52:11 +0700

    feat/ui_nguoidung_done

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=LoaiXe}/{action=Index}/{id?}")
+    pattern: "{controller=NguoiDung}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.Run();
